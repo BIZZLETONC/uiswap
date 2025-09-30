02749c386d68ea6d800e4fa869ac81fa8e578c37
GMonchain <gmonchain@gmail.com>
2025-09-30 22:19:40 +0700

    Docs: Clarify comment in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-// Helper function for combining CSS classes
+// A utility function for combining CSS classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
