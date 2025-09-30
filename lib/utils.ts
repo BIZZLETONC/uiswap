0fbb3e890f5103d02ccaeda01e70ca7f0b52f1c2
GMonchain <gmonchain@gmail.com>
2025-09-30 18:55:16 +0700

    Commit 18/20: Add comment to lib/utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  // Commit 18: Added a comment to the cn function in utils.ts.
   return twMerge(clsx(inputs))
 }
