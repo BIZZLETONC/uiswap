b78d60e359240f729b3a7946914528f86feb9b49
GMonchain <gmonchain@gmail.com>
2025-09-30 22:19:58 +0700

    Revert: Remove temporary todo comment

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,6 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 // A utility function for combining CSS classes
-// todo: remove later
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
