aaade3e89f0a1b9d04c3244dd8bd63dbc7430eb9
GMonchain <gmonchain@gmail.com>
2025-09-30 19:06:01 +0700

    Docs: Add comment for cn utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Utility function for combining CSS classes
 export function cn(...inputs: ClassValue[]) {
-  // Commit 18: Added a comment to the cn function in utils.ts.
   return twMerge(clsx(inputs))
 }
