f7e48398e29ee851ed9435c5eb400948d4b42a0c
GMonchain <gmonchain@gmail.com>
2025-09-30 22:19:49 +0700

    Chore: Add temporary todo comment

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 // A utility function for combining CSS classes
+// todo: remove later
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
