961dcee331e040f4c15ba38cf23723ff64052192
GMonchain <gmonchain@gmail.com>
2025-09-30 22:19:32 +0700

    Revert: Remove temporary log from cn function

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,5 @@ import { twMerge } from "tailwind-merge"
 
 // Helper function for combining CSS classes
 export function cn(...inputs: ClassValue[]) {
-  console.log("cn function called with inputs:", inputs);
   return twMerge(clsx(inputs))
 }
