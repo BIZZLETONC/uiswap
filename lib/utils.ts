9afd992f74eb5c7c2c3193e753d18ed1be7625b4
GMonchain <gmonchain@gmail.com>
2025-09-30 22:19:23 +0700

    Feat: Add temporary log in cn function

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,5 +3,6 @@ import { twMerge } from "tailwind-merge"
 
 // Helper function for combining CSS classes
 export function cn(...inputs: ClassValue[]) {
+  console.log("cn function called with inputs:", inputs);
   return twMerge(clsx(inputs))
 }
